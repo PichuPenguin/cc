08c7ecec545801e7f28c13b57598b08661074953
PichuPenguin <92007519+PichuPenguin@users.noreply.github.com>
2025-01-23 11:34:16 -0800

    Update main.js
    fixed heralds

--- main.js
+++ main.js
@@ -2496,9 +2496,7 @@ Game.Launch=function()
 		{
 			if (!App) ajax('/patreon/grab.php',Game.GrabDataResponse);
 			else App.grabData(function(res){
-				Game.heralds=res?(res.playersN||1):1;
-				Game.heralds=Math.max(0,Math.min(100,Math.ceil(Game.heralds/100*100)/100));
-				l('heraldsAmount').textContent=Math.floor(Game.heralds);
+				Game.heralds=100;
 			});
 		}
 		Game.GrabDataResponse=function(response)
